c611736b780f918313682f491bdeebeb82763964
Gabriel28 <nasc.gabriel.1997@gmail.com>
2019-04-17 18:55:28 -0300

    adicionada tabelas para adicionar o produto

--- produto-formulario.php
+++ produto-formulario.php
@@ -2,11 +2,20 @@
 
 <h1>Formulário de cadastro</h1>
 <form action="adiciona-produto.php">
-    Nome: <input type="text" name="nome" /><br/>
-    Preço: <input type="number" name="preco" /><br/>
-
-    <button type="submit">Cadastrar</button>
-
+    <table class="table">
+        <tr>
+            <td>Nome: </td>
+            <td><input type="text" class="form-control" name="nome" /></td>
+        </tr>
+        <tr>
+            <td>Preço: </td>
+            <td><input type="number" class="form-control" name="preco" /></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><button type="submit" class="btn btn-primary">Cadastrar</button></td>
+        </tr>
+    </table>
 </form>
 
 <?php include("rodape.php"); ?>
